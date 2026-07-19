c1724e86a4e7a4a235d059c3680e575781d7e78a
Angel Chakmaa <angelchakma@airs-MacBook-Air.local>
2026-07-19 16:09:25 +0600

    OOP inheritance

---
+++ OOP - Object Oriented Programming/Classes & Object.py
@@ -0,0 +1,37 @@
+
+#clases and object from hablu programmer
+
+class Angel_Chakma:
+    name = ""
+    age = 0
+    
+chakma = Angel_Chakma()
+Marma = Angel_Chakma()
+
+chakma.name = "Angel Chakma from rangamati"
+chakma.age =18
+print(chakma.name)
+print(chakma.age)
+
+Marma.name = "Saifi marma"
+print(Marma.name)
+#From english instructor Bro Code
+
+class programmer:
+
+    name = name
+    age = age
+    City = City
+    country = country
+
+    def __init__(self,name,age, city,country):
+
+        self.name = None
+        self.age = None
+        self.City = None
+        self.country = None
+
+    def Working_programmers (self):
+        print("This programmer are working")
+    def programmer_age(self):
+        print("This programmer age is:")
